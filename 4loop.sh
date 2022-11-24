1fd7502f36f4b0a28ca862293d1b26e4a636da75
Obinna <wiz.obi7509@gmail.com>
2022-11-24 22:53:55 +0000

    4loop file created

---
+++ 4loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#This script will be used to calculate print numbers between 200 to 1000
+#Good for load balancing and check.
+echo "For loop started
+if (( a=200;a>1001;a++ ))
+do
+echo $a
+done
+echo "for loop is done"
+
+
